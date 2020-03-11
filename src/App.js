3d5c78c74b6b5ad3608b68e6f0d9288627caa395
shourovys <shourovys@gmail.com>
2020-03-11 12:20:34 +0600

    semelar look & diffrernt data added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const productItem = [
+    { name: "Photo shop", price: "4404.99" },
+    { name: "Photo sioosp", price: "44840.99" },
+    { name: "Photo sholdlp", price: "478440.99" },
+    { name: "Photo shodsklp", price: "44950.99" }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Product product1={productItem[0]}></Product>
+      <Product product1={productItem[1]}></Product>
+      <Product product1={productItem[2]}></Product>
+      <Product product1={productItem[3]}></Product>
+      <Product product1={productItem[2]}></Product>
+      <Product product1={productItem[2]}></Product>
+      <Product product1={productItem[2]}></Product>
+      <Product product1={productItem[2]}></Product>
+      <Product product1={productItem[2]}></Product>
+      <Product product1={productItem[2]}></Product>
+    </div>
+  );
+}
+
+function Product(props) {
+  const { name, price } = props.product1;
+  console.log(name, price);
+
+  return (
+    <div className="box">
+      <h3 style={{ fontSize: "1.9rem", margin: "8px 5px" }}>{name}</h3>
+
+      <h1>{price}</h1>
+
+      <button className="buy-btn">buy now</button>
     </div>
   );
 }
